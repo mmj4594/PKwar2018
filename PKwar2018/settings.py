0b9fced68b6082fabfc4d77811a379fd748411e5
mmj <csmmj4594@postech.ac.kr>
2018-09-02 23:42:44 +0900

    경기 중 이벤트 추가

--- PKwar2018/settings.py
+++ PKwar2018/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mte)ju@v5xc_4pt=r(g(s)ceo)%3l@qk(*vsx73jda@i_5*bd)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,4 +119,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
